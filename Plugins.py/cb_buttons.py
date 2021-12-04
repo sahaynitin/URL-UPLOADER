a9964cf7bb93b2c12a12e3fb019e2b248ab642a1
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-12-04 20:13:31 +0530

    Update cb_buttons.py

--- Plugins.py/cb_buttons.py
+++ Plugins.py/cb_buttons.py
@@ -5,8 +5,8 @@ if bool(os.environ.get("WEBHOOK", False)):
 else:
     from config import Config
 
-from plugins.youtube_dl_button import youtube_dl_call_back
-from plugins.dl_button import ddl_call_back
+from Plugins.youtube_dl_button import youtube_dl_call_back
+from Plugins.dl_button import ddl_call_back
 
 from pyrogram import Client
 
